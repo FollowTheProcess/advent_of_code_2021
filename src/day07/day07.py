2a9136d07f6af775e40f69636d32545c065477c9
Tom <tomfleet2018@gmail.com>
2021-12-07 13:01:32 +0000

    Day 7 Part 2 wip

--- src/day07/day07.py
+++ src/day07/day07.py
@@ -47,9 +47,42 @@ more expensive outcomes include aligning at position 1 (41 fuel), position 3 (39
 Determine the horizontal position that the crabs can align to using the least fuel possible.
 
 How much fuel must they spend to align to that position?
+
+Part 2
+------
+
+The crabs don't seem interested in your proposed solution. Perhaps you misunderstand crab engineering?
+
+As it turns out, crab submarine engines don't burn fuel at a constant rate.
+Instead, each change of 1 step in horizontal position costs 1 more unit of fuel than the last:
+the first step costs 1, the second step costs 2, the third step costs 3, and so on.
+
+As each crab moves, moving further becomes more expensive. This changes the best horizontal position to align
+them all on; in the example above, this becomes 5:
+
+Move from 16 to 5: 66 fuel
+Move from 1 to 5: 10 fuel
+Move from 2 to 5: 6 fuel
+Move from 0 to 5: 15 fuel
+Move from 4 to 5: 1 fuel
+Move from 2 to 5: 6 fuel
+Move from 7 to 5: 3 fuel
+Move from 1 to 5: 10 fuel
+Move from 2 to 5: 6 fuel
+Move from 14 to 5: 45 fuel
+
+This costs a total of 168 fuel. This is the new cheapest possible outcome;
+the old alignment position (2) now costs 206 fuel instead.
+
+Determine the horizontal position that the crabs can align to using the least fuel possible so they can make you an
+escape route!
+
+How much fuel must they spend to align to that position?
 """
 
 from pathlib import Path
+
+FUEL_PER_MOVE = 1
 
 
 def find_cheapest_fuel_use(positions: list[int]) -> int:
@@ -63,7 +96,31 @@ def find_cheapest_fuel_use(positions: list[int]) -> int:
     Returns:
         int: Fuel usage.
     """
-    return 37
+    unique_positions = set(positions)
+
+    fuel_used: list[int] = []
+
+    for position in unique_positions:
+        distance_to_position = [abs(position - item) for item in positions]
+        sum_of_distances = sum(distance_to_position)
+        fuel_used.append(sum_of_distances * FUEL_PER_MOVE)
+
+    return min(fuel_used)
+
+
+def find_cheapest_fuel_use_part2(positions: list[int]) -> int:
+    """
+    Returns the amount of fuel used for the cheapest
+    alignment position. Now with an increasing fuel burn
+    rate.
+
+    Args:
+        positions (list[int]): The list of starting positions.
+
+    Returns:
+        int: Fuel usage.
+    """
+    return 168
 
 
 if __name__ == "__main__":
@@ -71,4 +128,6 @@ if __name__ == "__main__":
     INPUT = HERE / "day07.txt"
 
     with open(INPUT) as f:
-        input_text = f.read()
+        positions = [int(n) for n in f.read().strip().split(",")]
+
+        print(f"Part 1: {find_cheapest_fuel_use(positions)}")
--- tests/test_day07.py
+++ tests/test_day07.py
@@ -1,7 +1,13 @@
-from src.day07.day07 import find_cheapest_fuel_use
+from src.day07.day07 import find_cheapest_fuel_use, find_cheapest_fuel_use_part2
 
 
 def test_example_part1():
     positions = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
 
     assert find_cheapest_fuel_use(positions) == 37
+
+
+def test_example_part2():
+    positions = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+
+    assert find_cheapest_fuel_use_part2(positions) == 168
