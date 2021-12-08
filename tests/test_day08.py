9bf67c37e7075f4243b03a1bfcdca40ab820544e
Tom <tomfleet2018@gmail.com>
2021-12-08 08:05:11 +0000

    Day 8 WIP

---
+++ tests/test_day08.py
@@ -0,0 +1,6 @@
+from src.day08.day08 import ALL_SEGMENTS, DIGITS
+
+
+def test_constants():
+    assert ALL_SEGMENTS == 7
+    assert DIGITS == {0: 6, 1: 2, 2: 5, 3: 5, 4: 4, 5: 5, 6: 6, 7: 3, 8: 7, 9: 6}
